c9e80ccfb6fdf76b83a339e44d4ce8d4da185c8f
SNUFKIN-DINEROS <jerzelldineros@gmail.com>
2026-09-18 08:38:18 +0800

    Update DINEROS.java

--- app/src/test/java/QUARTER2/DINEROS.java
+++ app/src/test/java/QUARTER2/DINEROS.java
@@ -1,4 +1,19 @@
 package QUARTER2;
 
-public class DINEROS {
+import org.junit.Test;
+
+public class DinerosProfile {
+
+    @Test
+    public void printMyProfile() {
+        String myName = "Jerzell Lourisse R. Dineros";
+        String petName = "Bella";
+        String favFood = "omelette";
+        int myAge = 16;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hi! My name is " + myName + " and I'm " + myAge + " years old.");
+        System.out.println("I have a cute pet named " + petName + ".");
+        System.out.println("My favorite food is " + favFood + " — I could eat it everyday!");
+    }
 }
