9a5f93a768ee1b70506586aaca8f21f01f6bc411
SNUFKIN-DINEROS <jerzelldineros@gmail.com>
2026-09-20 08:37:04 +0800

    Add JUnit test for printing digital profile
    Added a JUnit test method to print a digital profile with personal details.

--- app/src/test/java/QUARTER2/DINEROS.java
+++ app/src/test/java/QUARTER2/DINEROS.java
@@ -1,4 +1,16 @@
 package QUARTER2;
-
+import org.junit.Test;
 public class DINEROS {
+
+    @Test
+    public void printMYprofile() {
+        String myName = "DINEROS";
+        String petname = "BELLA";
+        String favfood = "OMELETTE";
+        int myAge = 16;
+
+        System.out.println("---MY DIGITAL PROFILE---");
+        System.out.println("Hello, my name is" + myName + "and I am" + myAge + "years old");
+        System.out.println("It's black, that's my pet color" + petname + "BLACKY");
+        System.out.println("Everything tastes better when you're hungry" + favfood + "YUMMY!!");}
 }
