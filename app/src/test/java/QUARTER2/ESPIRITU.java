4067156b87ffe5bedf6c5e2dfc6618dc28296b89
Halili, Ronazld R. <ronaldhalili05@gmail.com>
2026-09-25 11:55:43 +0800

    Added Espiritu's profile test

--- app/src/test/java/QUARTER2/ESPIRITU.java
+++ app/src/test/java/QUARTER2/ESPIRITU.java
@@ -1,5 +1,7 @@
-package QUARTER2;
+package quarter2;
+
 import org.junit.Test;
+
 public class ESPIRITU {
 
     @Test
@@ -10,7 +12,8 @@ public class ESPIRITU {
         int myAge = 17;
 
         System.out.println("---MY DIGITAL PROFILE---");
-        System.out.println("Hello, my name is" + myName + "and I am" + myAge + "years old");
-        System.out.println("I have a wonderful pet named" + petname + ".");
-        System.out.println("If i could eat, I would eat" + favfood + "everyday!");}
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old");
+        System.out.println("I have a wonderful pet named " + petname + ".");
+        System.out.println("If I could eat, I would eat " + favfood + " everyday!");
     }
+}
