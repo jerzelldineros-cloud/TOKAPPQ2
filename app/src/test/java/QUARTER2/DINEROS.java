1dd9510d9c2b1aba2f256ff15713d5810a8f5e2d
SNUFKIN-DINEROS <jerzelldineros@gmail.com>
2026-09-25 15:16:36 +0800

    name and add profile tes

--- app/src/test/java/QUARTER2/DINEROS.java
+++ app/src/test/java/QUARTER2/DINEROS.java
@@ -1,4 +1,19 @@
-package quarter2;
+package QUARTER2;
+
+import org.junit.Test;
 
 public class DINEROS {
+
+    @Test
+    public void printMYprofile() {
+        String myName = "DINEROS";
+        String petname = "BELLA";
+        String favfood = "OMELETTE";
+        int myAge = 16;
+
+        System.out.println("---MY DIGITAL PROFILE---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old");
+        System.out.println("My pet's name is " + petname + " BELLA");
+        System.out.println("Omelette being the best food ever! " + favfood + " UMMEEE!!");
+    }
 }
